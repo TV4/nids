efa497e6b8a259bf10f35443b9feae46e1b9a667
Peter Hellberg <peter@c7.se>
2016-06-29 16:30:37 +0200

    Refactored to support functional options

--- nid.go
+++ nid.go
@@ -32,147 +32,325 @@ import (
 	"strings"
 )
 
-var (
-	validPattern  = regexp.MustCompile(`\A[0-9a-z-]*\z`)
-	squishPattern = regexp.MustCompile(`\s+`)
-	stripPattern  = regexp.MustCompile(`[^0-9a-z-]`)
-	dashPattern   = regexp.MustCompile("-{1,}")
+// Default is the default configuration used when calling nid.Case and nid.Possible
+var Default = New()
 
-	dashSpace = strings.NewReplacer("-", " ", "_", " ", "–", " ", "—", " ")
-)
+// WithÅÄÖ allows åäö in nids (and replaces ø with ö)
+var WithÅÄÖ = New(AllowÅÄÖ)
 
 // Case returns a nid based on the input text
 func Case(text string) string {
-	if text == "" {
-		return ""
-	}
-
-	return strip(transliterate(squish(prepare(text))))
+	return Default.Case(text)
 }
 
 // Possible checks if a candidate string is a possible nid
 func Possible(candidate string) bool {
+	return Default.Possible(candidate)
+}
+
+// Nid contains the configuration used to create and validate nids
+type Nid struct {
+	ValidPattern     *regexp.Regexp
+	SquishPattern    *regexp.Regexp
+	StripPattern     *regexp.Regexp
+	DashPattern      *regexp.Regexp
+	DashSpace        *strings.Replacer
+	Transliterations *strings.Replacer
+}
+
+// New returns a *Nid that implements the Interface
+func New(options ...func(*Nid)) *Nid {
+	n := &Nid{
+		ValidPattern:     validPattern,
+		SquishPattern:    squishPattern,
+		StripPattern:     stripPattern,
+		DashPattern:      dashPattern,
+		DashSpace:        dashSpace,
+		Transliterations: transliterations,
+	}
+
+	for _, option := range options {
+		option(n)
+	}
+
+	return n
+}
+
+// AllowÅÄÖ is a functional option that can be used in New
+func AllowÅÄÖ(n *Nid) {
+	SetValidPattern(regexp.MustCompile(`\A[0-9a-zåäö-]*\z`))(n) // Note: Allows empty nids
+	SetStripPattern(regexp.MustCompile(`[^0-9a-zåäö-]`))(n)
+	SetTransliterations(transliterationsWithÅÄÖ)(n)
+}
+
+// SetValidPattern is a functional option that can be used in New
+func SetValidPattern(r *regexp.Regexp) func(*Nid) {
+	return func(n *Nid) {
+		if r != nil {
+			n.ValidPattern = r
+		}
+	}
+}
+
+// SetStripPattern is a functional option that can be used in New
+func SetStripPattern(r *regexp.Regexp) func(*Nid) {
+	return func(n *Nid) {
+		if r != nil {
+			n.StripPattern = r
+		}
+	}
+}
+
+// SetTransliterations is a functional option that can be used in New
+func SetTransliterations(r *strings.Replacer) func(*Nid) {
+	return func(n *Nid) {
+		if r != nil {
+			n.Transliterations = r
+		}
+	}
+}
+
+// Case returns a nid based on the input text
+func (n *Nid) Case(text string) string {
+	if text == "" {
+		return ""
+	}
+
+	return n.strip(n.transliterate(n.squish(n.prepare(text))))
+}
+
+// Possible checks if a candidate string is a possible nid
+func (n *Nid) Possible(candidate string) bool {
 	if strings.Contains(candidate, "--") {
 		return false
 	}
 
-	return validPattern.MatchString(candidate)
+	return n.ValidPattern.MatchString(candidate)
 }
 
-func strip(s string) string {
-	return dashPattern.ReplaceAllString(stripPattern.ReplaceAllString(s, ""), "-")
+func (n *Nid) strip(s string) string {
+	return n.DashPattern.ReplaceAllString(n.StripPattern.ReplaceAllString(s, ""), "-")
 }
 
-func transliterate(s string) string {
-	return transliterations.Replace(s)
+func (n *Nid) transliterate(s string) string {
+	return n.Transliterations.Replace(s)
 }
 
-func squish(s string) string {
-	return squishPattern.ReplaceAllString(s, " ")
+func (n *Nid) squish(s string) string {
+	return n.SquishPattern.ReplaceAllString(s, " ")
 }
 
-func prepare(s string) string {
-	return strings.TrimSpace(dashSpace.Replace(strings.ToLower(s)))
+func (n *Nid) prepare(s string) string {
+	return strings.TrimSpace(n.DashSpace.Replace(strings.ToLower(s)))
 }
 
-var transliterations = strings.NewReplacer(
-	" ", "-",
-	"_", "-",
-	"×", "x",
-	"ß", "ss",
-	"à", "a",
-	"á", "a",
-	"â", "a",
-	"ã", "a",
-	"ä", "a",
-	"å", "a",
-	"æ", "a",
-	"ç", "c",
-	"è", "e",
-	"é", "e",
-	"ê", "e",
-	"ë", "e",
-	"ì", "i",
-	"í", "i",
-	"î", "i",
-	"ï", "i",
-	"ð", "d",
-	"ñ", "n",
-	"ò", "o",
-	"ó", "o",
-	"ô", "o",
-	"õ", "o",
-	"ö", "o",
-	"ø", "o",
-	"ù", "u",
-	"ú", "u",
-	"û", "u",
-	"ü", "u",
-	"ý", "y",
-	"þ", "th",
-	"ÿ", "y",
-	"ā", "a",
-	"ă", "a",
-	"ą", "a",
-	"ć", "c",
-	"ĉ", "c",
-	"ċ", "c",
-	"č", "c",
-	"ď", "d",
-	"đ", "d",
-	"ē", "e",
-	"ĕ", "e",
-	"ė", "e",
-	"ę", "e",
-	"ě", "e",
-	"ĝ", "g",
-	"ğ", "g",
-	"ġ", "g",
-	"ģ", "g",
-	"ĥ", "h",
-	"ħ", "h",
-	"ĩ", "i",
-	"ī", "i",
-	"ĭ", "i",
-	"į", "i",
-	"ı", "i",
-	"ĳ", "ij",
-	"ĵ", "j",
-	"ķ", "k",
-	"ĸ", "k",
-	"ĺ", "l",
-	"ļ", "l",
-	"ľ", "l",
-	"ŀ", "l",
-	"ł", "l",
-	"ń", "n",
-	"ņ", "n",
-	"ň", "n",
-	"ŉ", "'n",
-	"ŋ", "ng",
-	"ō", "o",
-	"ŏ", "o",
-	"ő", "o",
-	"œ", "oe",
-	"ŕ", "r",
-	"ŗ", "r",
-	"ř", "r",
-	"ś", "s",
-	"ŝ", "s",
-	"ş", "s",
-	"š", "s",
-	"ţ", "t",
-	"ť", "t",
-	"ŧ", "t",
-	"ũ", "u",
-	"ū", "u",
-	"ŭ", "u",
-	"ů", "u",
-	"ű", "u",
-	"ų", "u",
-	"ŵ", "w",
-	"ŷ", "y",
-	"ź", "z",
-	"ż", "z",
-	"ž", "z",
+var (
+	validPattern     = regexp.MustCompile(`\A[0-9a-z-]{1,}\z`)
+	squishPattern    = regexp.MustCompile(`\s+`)
+	stripPattern     = regexp.MustCompile(`[^0-9a-z-]`)
+	dashPattern      = regexp.MustCompile("-{1,}")
+	dashSpace        = strings.NewReplacer("-", " ", "_", " ", "–", " ", "—", " ")
+	transliterations = strings.NewReplacer(
+		" ", "-",
+		"_", "-",
+		"×", "x",
+		"ß", "ss",
+		"à", "a",
+		"á", "a",
+		"â", "a",
+		"ã", "a",
+		"ä", "a",
+		"å", "a",
+		"æ", "a",
+		"ç", "c",
+		"è", "e",
+		"é", "e",
+		"ê", "e",
+		"ë", "e",
+		"ì", "i",
+		"í", "i",
+		"î", "i",
+		"ï", "i",
+		"ð", "d",
+		"ñ", "n",
+		"ò", "o",
+		"ó", "o",
+		"ô", "o",
+		"õ", "o",
+		"ö", "o",
+		"ø", "o",
+		"ù", "u",
+		"ú", "u",
+		"û", "u",
+		"ü", "u",
+		"ý", "y",
+		"þ", "th",
+		"ÿ", "y",
+		"ā", "a",
+		"ă", "a",
+		"ą", "a",
+		"ć", "c",
+		"ĉ", "c",
+		"ċ", "c",
+		"č", "c",
+		"ď", "d",
+		"đ", "d",
+		"ē", "e",
+		"ĕ", "e",
+		"ė", "e",
+		"ę", "e",
+		"ě", "e",
+		"ĝ", "g",
+		"ğ", "g",
+		"ġ", "g",
+		"ģ", "g",
+		"ĥ", "h",
+		"ħ", "h",
+		"ĩ", "i",
+		"ī", "i",
+		"ĭ", "i",
+		"į", "i",
+		"ı", "i",
+		"ĳ", "ij",
+		"ĵ", "j",
+		"ķ", "k",
+		"ĸ", "k",
+		"ĺ", "l",
+		"ļ", "l",
+		"ľ", "l",
+		"ŀ", "l",
+		"ł", "l",
+		"ń", "n",
+		"ņ", "n",
+		"ň", "n",
+		"ŉ", "'n",
+		"ŋ", "ng",
+		"ō", "o",
+		"ŏ", "o",
+		"ő", "o",
+		"œ", "oe",
+		"ŕ", "r",
+		"ŗ", "r",
+		"ř", "r",
+		"ś", "s",
+		"ŝ", "s",
+		"ş", "s",
+		"š", "s",
+		"ţ", "t",
+		"ť", "t",
+		"ŧ", "t",
+		"ũ", "u",
+		"ū", "u",
+		"ŭ", "u",
+		"ů", "u",
+		"ű", "u",
+		"ų", "u",
+		"ŵ", "w",
+		"ŷ", "y",
+		"ź", "z",
+		"ż", "z",
+		"ž", "z",
+	)
+
+	transliterationsWithÅÄÖ = strings.NewReplacer(
+		" ", "-",
+		"_", "-",
+		"×", "x",
+		"ß", "ss",
+		"à", "a",
+		"á", "a",
+		"â", "a",
+		"ã", "a",
+		"ä", "ä",
+		"å", "å",
+		"æ", "ä",
+		"ç", "c",
+		"è", "e",
+		"é", "e",
+		"ê", "e",
+		"ë", "e",
+		"ì", "i",
+		"í", "i",
+		"î", "i",
+		"ï", "i",
+		"ð", "d",
+		"ñ", "n",
+		"ò", "o",
+		"ó", "o",
+		"ô", "o",
+		"õ", "o",
+		"ö", "ö",
+		"ø", "ö",
+		"ù", "u",
+		"ú", "u",
+		"û", "u",
+		"ü", "u",
+		"ý", "y",
+		"þ", "th",
+		"ÿ", "y",
+		"ā", "a",
+		"ă", "a",
+		"ą", "a",
+		"ć", "c",
+		"ĉ", "c",
+		"ċ", "c",
+		"č", "c",
+		"ď", "d",
+		"đ", "d",
+		"ē", "e",
+		"ĕ", "e",
+		"ė", "e",
+		"ę", "e",
+		"ě", "e",
+		"ĝ", "g",
+		"ğ", "g",
+		"ġ", "g",
+		"ģ", "g",
+		"ĥ", "h",
+		"ħ", "h",
+		"ĩ", "i",
+		"ī", "i",
+		"ĭ", "i",
+		"į", "i",
+		"ı", "i",
+		"ĳ", "ij",
+		"ĵ", "j",
+		"ķ", "k",
+		"ĸ", "k",
+		"ĺ", "l",
+		"ļ", "l",
+		"ľ", "l",
+		"ŀ", "l",
+		"ł", "l",
+		"ń", "n",
+		"ņ", "n",
+		"ň", "n",
+		"ŉ", "'n",
+		"ŋ", "ng",
+		"ō", "o",
+		"ŏ", "o",
+		"ő", "o",
+		"œ", "oe",
+		"ŕ", "r",
+		"ŗ", "r",
+		"ř", "r",
+		"ś", "s",
+		"ŝ", "s",
+		"ş", "s",
+		"š", "s",
+		"ţ", "t",
+		"ť", "t",
+		"ŧ", "t",
+		"ũ", "u",
+		"ū", "u",
+		"ŭ", "u",
+		"ů", "u",
+		"ű", "u",
+		"ų", "u",
+		"ŵ", "w",
+		"ŷ", "y",
+		"ź", "z",
+		"ż", "z",
+		"ž", "z",
+	)
 )
--- nid_test.go
+++ nid_test.go
@@ -2,71 +2,84 @@ package nid
 
 import (
 	"fmt"
+	"regexp"
 	"testing"
 )
 
 func TestCase(t *testing.T) {
-	for _, tt := range []struct {
+	for i, tt := range []struct {
 		in   string
 		want string
+		nid  *Nid
 	}{
 		// should nid case "" to ""
-		{"", ""},
+		{"", "", Default},
 
 		// should change space to dash
-		{"foo bar bee", "foo-bar-bee"},
+		{"foo bar bee", "foo-bar-bee", Default},
 
-		// should downcase all chars in the Swedish alphabet
-		{"ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ", "abcdefghijklmnopqrstuvwxyzaao"},
+		// should downcase all chars in the Swedish alphabet if allowing åäö
+		{"ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ", "abcdefghijklmnopqrstuvwxyzåäö", WithÅÄÖ},
+
+		// should downcase all chars and transliterate
+		{"ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ", "abcdefghijklmnopqrstuvwxyzaao", Default},
+
+		// should downcase all chars matched by the strip pattern
+		{"ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ", "abcdefghijklmnopqrstuvwxyz", New(AllowÅÄÖ, SetStripPattern(regexp.MustCompile(`[^a-z-]`)))},
 
 		// should transliterate
-		{"ÀÁÂÃÄÅÆÇ_ÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜ", "aaaaaaac-eeeeiiiidnoooooxouuuu"},
-		{"ÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúû", "ythssaaaaaaaceeeeiiiidnoooooouuu"},
-		{"üýþÿĀāĂăĄąĆćĈĉĊċČčĎďĐđĒēĔĕĖėĘę", "uythyaaaaaaccccccccddddeeeeeeee"},
-		{"ĚěĜĝĞğĠġĢģĤĥĦħĨĩĪīĬĭĮįİıĲĳĴĵĶķ", "eegggggggghhhhiiiiiiiiiiijijjjkk"},
-		{"ĸĹĺĻļĽľĿŀŁłŃńŅņŇňŉŊŋŌōŎŏŐőŒœŔŕ", "kllllllllllnnnnnnnngngoooooooeoerr"},
-		{"ŖŗŘřŚśŜŝŞşŠšŢţŤťŦŧŨũŪūŬŭŮůŰűŲų", "rrrrssssssssttttttuuuuuuuuuuuu"},
-		{"ŴŵŶŷŸŹźŻżŽž", "wwyyyzzzzzz"},
+		{"ÀÁÂÃÄÅÆÇ_ÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜ", "aaaaäåäc-eeeeiiiidnooooöxöuuuu", WithÅÄÖ},
+		{"ÀÁÂÃÄÅÆÇ_ÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜ", "aaaaaaac-eeeeiiiidnoooooxouuuu", Default},
+		{"ÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúû", "ythssaaaaäåäceeeeiiiidnooooööuuu", WithÅÄÖ},
+		{"ÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúû", "ythssaaaaaaaceeeeiiiidnoooooouuu", Default},
+		{"üýþÿĀāĂăĄąĆćĈĉĊċČčĎďĐđĒēĔĕĖėĘę", "uythyaaaaaaccccccccddddeeeeeeee", Default},
+		{"ĚěĜĝĞğĠġĢģĤĥĦħĨĩĪīĬĭĮįİıĲĳĴĵĶķ", "eegggggggghhhhiiiiiiiiiiijijjjkk", Default},
+		{"ĸĹĺĻļĽľĿŀŁłŃńŅņŇňŉŊŋŌōŎŏŐőŒœŔŕ", "kllllllllllnnnnnnnngngoooooooeoerr", Default},
+		{"ŖŗŘřŚśŜŝŞşŠšŢţŤťŦŧŨũŪūŬŭŮůŰűŲų", "rrrrssssssssttttttuuuuuuuuuuuu", Default},
+		{"ŴŵŶŷŸŹźŻżŽž", "wwyyyzzzzzz", Default},
 
 		// should ignore all chars not included in a-z, åäö, -, 0-9
-		{"kale8^79'0-", "kale8790"},
+		{"kale8^79'0-", "kale8790", Default},
+		{"kale8^79'0-", "kale", New(SetStripPattern(regexp.MustCompile(`[^a-z]`)))},
 
 		// should convert diacritical characters
-		{"Dürén Ibrahimović", "duren-ibrahimovic"},
+		{"Dürén Ibrahimović", "duren-ibrahimovic", Default},
 
-		// does not preserve åäö
-		{"ÅÄÖåäö", "aaoaao"},
+		// preserves åäö ÅÄÖ if allowing åäö
+		{"ÅÄÖåäö", "åäöåäö", WithÅÄÖ},
 
 		// converts `¨´^
-		{"ÈÉËÊèéëê", "eeeeeeee"},
-		{"ÀÁÂàáâ", "aaaaaa"},
-		{"Üü", "uu"},
-		{"ČĆÇčćç", "cccccc"},
-		{"Ññ", "nn"},
-		{"Ïï", "ii"},
-		{"ÆØæø", "aoao"},
+		{"ÈÉËÊèéëê", "eeeeeeee", Default},
+		{"ÀÁÂàáâ", "aaaaaa", Default},
+		{"Üü", "uu", Default},
+		{"ČĆÇčćç", "cccccc", Default},
+		{"Ññ", "nn", Default},
+		{"Ïï", "ii", Default},
+		{"ÆØæø", "aoao", Default},
+		{"ÆØæø", "äöäö", WithÅÄÖ},
 
 		// converts _ to -
-		{"Let's_Dance", "lets-dance"},
-		{"N___F5__9hf3m2iDyO4F__rjyD", "n-f5-9hf3m2idyo4f-rjyd"},
+		{"Let's_Dance", "lets-dance", Default},
+		{"N___F5__9hf3m2iDyO4F__rjyD", "n-f5-9hf3m2idyo4f-rjyd", Default},
 
 		// removes -- from name
-		{"Let's -- da-da-dance", "lets-da-da-dance"},
+		{"Let's -- da-da-dance", "lets-da-da-dance", Default},
 
 		// removes surrounding and double space from name and tag
-		{" Fångarna     på  fortet   ", "fangarna-pa-fortet"},
+		{" Fångarna     på  fortet   ", "fangarna-pa-fortet", Default},
+		{" Fångarna     på  fortet   ", "fångarna-på-fortet", WithÅÄÖ},
 
 		// removes –, —
-		{"Arn – Tempelriddaren", "arn-tempelriddaren"},
-		{"Arn — Tempelriddaren", "arn-tempelriddaren"},
+		{"Arn – Tempelriddaren", "arn-tempelriddaren", Default},
+		{"Arn — Tempelriddaren", "arn-tempelriddaren", Default},
 
 		// replaces repeating dashes with single dash
-		{"Alvinnn!! & the Chipmunks", "alvinnn-the-chipmunks"},
-		{"alvinnn--the---chipmunks", "alvinnn-the-chipmunks"},
-		{"alvinnn--the-chipmunks", "alvinnn-the-chipmunks"},
+		{"Alvinnn!! & the Chipmunks", "alvinnn-the-chipmunks", Default},
+		{"alvinnn--the---chipmunks", "alvinnn-the-chipmunks", Default},
+		{"alvinnn--the-chipmunks", "alvinnn-the-chipmunks", Default},
 	} {
-		if got := Case(tt.in); got != tt.want {
-			t.Errorf(`nid.Case(%q) = %q, want %q`, tt.in, got, tt.want)
+		if got := tt.nid.Case(tt.in); got != tt.want {
+			t.Fatalf(`[%d] tt.nid.Case(%q) = %q, want %q`, i, tt.in, got, tt.want)
 		}
 	}
 }
@@ -78,25 +91,25 @@ func ExampleCase() {
 
 func BenchmarkPartialPrepare(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		prepare("Dürén Ibrahimović")
+		Default.prepare("Dürén Ibrahimović")
 	}
 }
 
 func BenchmarkPartialPrepareSquish(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		squish(prepare("Dürén Ibrahimović"))
+		Default.squish(Default.prepare("Dürén Ibrahimović"))
 	}
 }
 
 func BenchmarkPartialPrepareSquishTransliterate(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		transliterate(squish(prepare("Dürén Ibrahimović")))
+		Default.transliterate(Default.squish(Default.prepare("Dürén Ibrahimović")))
 	}
 }
 
 func BenchmarkPartialPrepareSquishTransliterateStrip(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		strip(transliterate(squish(prepare("Dürén Ibrahimović"))))
+		Default.strip(Default.transliterate(Default.squish(Default.prepare("Dürén Ibrahimović"))))
 	}
 }
 
@@ -128,29 +141,36 @@ func TestPossible(t *testing.T) {
 	for _, tt := range []struct {
 		in   string
 		want bool
+		nid  *Nid
 	}{
-		// accepts nothingness
-		{"", true},
+		// does not accept nothingness by default
+		{"", false, Default},
 
-		// does not accept åäö
-		{"räksmörgås", false},
+		// accepts nothingness when allowing åäö
+		{"", true, WithÅÄÖ},
+
+		// accepts åäö
+		{"räksmörgås", true, WithÅÄÖ},
+
+		// does not accept åäö if we change the valid pattern
+		{"räksmörgås", false, Default},
 
 		// rejects double dashes
-		{"foo--bar", false},
+		{"foo--bar", false, Default},
 
 		// rejects diacritical marks
-		{"dürén-ibrahimović", false},
+		{"dürén-ibrahimović", false, Default},
 
 		// rejects non-letters
-		{"foo bar", false},
-		{"foo/bar", false},
-		{"foo\n", false},
+		{"foo bar", false, Default},
+		{"foo/bar", false, Default},
+		{"foo\n", false, Default},
 
 		// rejects upper-case letters
-		{"FOO", false},
+		{"FOO", false, Default},
 	} {
-		if got := Possible(tt.in); got != tt.want {
-			t.Errorf(`nid.Possible(%q) = %v, want %v`, tt.in, got, tt.want)
+		if got := tt.nid.Possible(tt.in); got != tt.want {
+			t.Errorf(`tt.nid.Possible(%q) = %v, want %v`, tt.in, got, tt.want)
 		}
 	}
 }
@@ -167,7 +187,7 @@ func TestSquish(t *testing.T) {
 	}{
 		{"foo  bar   baz", "foo bar baz"},
 	} {
-		if got := squish(tt.in); got != tt.want {
+		if got := Default.squish(tt.in); got != tt.want {
 			t.Errorf(`squish(%q) = %q, want %q`, tt.in, got, tt.want)
 		}
 	}
